f552288f35fa22abd8ae5b36164f20a288247cf5
Caroline Faria <caroline.gdfaria@gmail.com>
2023-05-24 22:13:36 -0300

    virando as cartas

--- main.js
+++ main.js
@@ -53,13 +53,24 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.getElementById('grid')
+const cardsChosen = []
 
 function createBoard() {
   for(let i = 0; i < cardArray.length; i++) {
     const card = document.createElement('img')
     card.setAttribute('src', 'images/blank.png')
     card.setAttribute('data-id', i)
+    card.addEventListener('click', flipCard)
     gridDisplay.appendChild(card)
   }
 }
 createBoard()
+
+function flipCard() {
+  let cardId = this.getAttribute('data-id')
+  cardsChosen.push(cardArray[cardId].name)
+  this.setAttribute('src', cardArray[cardId].img)
+  if(cardsChosen.length === 2) {
+    setTimeout(checkForMatch, 500)
+  }
+}
